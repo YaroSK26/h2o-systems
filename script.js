051b421aa2ca979a5488bca2e108a3e1e3673194
jaxo <jaroba0gmail.com>
2024-08-30 15:44:32 +0200

    google analytixs

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-import { inject } from "@vercel/analytics";
-
-inject();
-
 const translations = {
   sk: {
     link1: "Kto Sme?",
@@ -325,7 +321,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       updateLanguage(value);
 
-      localStorage.setItem("language", value);
+      localStorage.setItem('language', value);
     });
   });
 
@@ -345,12 +341,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  const savedLanguage = localStorage.getItem("language");
+  const savedLanguage = localStorage.getItem('language');
   if (savedLanguage) {
     updateLanguage(savedLanguage);
-    const option = document.querySelector(
-      `.select-items div[data-value="${savedLanguage}"]`
-    );
+    const option = document.querySelector(`.select-items div[data-value="${savedLanguage}"]`);
     if (option) {
       const imgSrc = option.getAttribute("data-img-src");
       selected.innerHTML = `<img src="${imgSrc}" alt="${savedLanguage}">`;
